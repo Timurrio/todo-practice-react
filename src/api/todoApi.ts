7e3d01b1cdf62aa9376cbf9517177e794487edbe
Tymur Myronenko <50523488+Timurrio@users.noreply.github.com>
2025-09-23 14:07:20 +0300

    added api logic

---
+++ src/api/todoApi.ts
@@ -0,0 +1,30 @@
+import api from './index';
+import type { Todo } from '../types/todo';
+
+export const createTodo = async (todoData: Omit<Todo, 'id'>) => {
+  const { data } = await api.post(`/todo`, todoData);
+  return data;
+};
+
+export const updateTodo = async (todoData: Todo) => {
+  const { data } = await api.put(`/todo/${todoData.id}`, {
+    text: todoData.text,
+    completed: todoData.completed,
+  });
+  return data;
+};
+
+export const deleteTodo = async (todoId: string) => {
+  const { data } = await api.delete(`/todo/${todoId}`);
+  return data;
+};
+
+export const getTodos = async () => {
+  const { data } = await api.get(`/todo`);
+  return data;
+};
+
+export const getTodoById = async (todoId: string) => {
+  const { data } = await api.get(`/todo${todoId}`);
+  return data;
+};
